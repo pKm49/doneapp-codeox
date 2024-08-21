c950d513c75a3a6e4db01e8049e61350a72eab95
Prasanth K M <prasanthkm49@gmail.com>
2024-08-21 17:57:46 +0530

    update

--- lib/feature_modules/auth/constants/http_request_endpoints.constant.auth.dart
+++ lib/feature_modules/auth/constants/http_request_endpoints.constant.auth.dart
@@ -1,4 +1,6 @@
 const String AuthHttpRequestEndpoint_Login = "/login";
 const String AuthHttpRequestEndpoint_Register = "/profile";
 const String AuthHttpRequestEndpoint_ResetPassword = "/reset_password";
+const String AuthHttpRequestEndpoint_GetMealCategories = "/meal_category";
+const String AuthHttpRequestEndpoint_GetMealsByCatory = "/meal/category/";
 
