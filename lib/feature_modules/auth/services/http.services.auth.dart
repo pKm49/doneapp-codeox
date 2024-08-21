1ad016a15299657f80dac774573f08cc2918d3ea
Prasanth K M <prasanthkm49@gmail.com>
2024-08-21 17:57:13 +0530

    update

--- lib/feature_modules/auth/services/http.services.auth.dart
+++ lib/feature_modules/auth/services/http.services.auth.dart
@@ -1,6 +1,8 @@
 import 'package:doneapp/feature_modules/auth/constants/http_request_endpoints.constant.auth.dart';
 import 'package:doneapp/feature_modules/auth/models/login_credential.model.auth.dart';
+import 'package:doneapp/feature_modules/auth/models/meal_item.model.auth.dart';
 import 'package:doneapp/feature_modules/auth/models/register_credential.model.auth.dart';
+import 'package:doneapp/shared_module/models/general_item.model.shared.dart';
  import 'package:doneapp/shared_module/models/http_response.model.shared.dart';
 import 'package:doneapp/shared_module/services/http-services/http_request_handler.service.shared.dart';
 import 'package:doneapp/shared_module/services/utility-services/toaster_snackbar_shower.service.shared.dart';
@@ -67,5 +69,54 @@ class AuthHttpService {
     }
   }
 
+  Future<List<GeneralItem>> getMealCategories() async {
+
+    try{
+      Map<String, dynamic> params = {};
+      AppHttpResponse response =
+      await getRequest(AuthHttpRequestEndpoint_GetMealCategories, params);
+
+      List<GeneralItem> tempMealCategories = [];
+      print("getMealCategories");
+      print(response.data);
+      if (response.statusCode == 200 && response.data != null) {
+        for (var i = 0; i < response.data.length; i++) {
+          tempMealCategories.add(mapGeneralItem(response.data[i]));
+        }
+      }
+      return tempMealCategories;
+
+    }catch  (e,st){
+      print(e);
+      print(st);
+      return [];
+    }
+  }
+
+  Future<List<MealItem>> getMealsByCategory(int categoryId ) async {
+
+    try{
+      AppHttpResponse response = await getRequest(
+          '$AuthHttpRequestEndpoint_GetMealsByCatory${categoryId}', null);
+
+      List<MealItem> tempSubscriptionPlans = [];
+      print("getMealsByCategory");
+      print(response.statusCode);
+      print(response.data);
+      if (response.statusCode == 200 && response.data != null) {
+        for (var i = 0; i < response.data.length; i++) {
+          tempSubscriptionPlans.add(mapMealItem(response.data[i]));
+        }
+      }
+
+      return tempSubscriptionPlans;
+
+    }catch  (e,st){
+      print(e);
+      print(st);
+      return [];
+    }
+  }
 
 }
+
