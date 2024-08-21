ac855eb0bac37bc0fce365c793f524ac7908d03e
Prasanth K M <prasanthkm49@gmail.com>
2024-08-21 17:58:01 +0530

    update

--- lib/feature_modules/plan_purchase/constants/http_request_endpoints.constant.plan_purchase.dart
+++ lib/feature_modules/plan_purchase/constants/http_request_endpoints.constant.plan_purchase.dart
@@ -3,5 +3,5 @@ const String SubscriptionsHttpRequestEndpoint_GetPlans =  "/plan_choice/";
 const String SubscriptionsHttpRequestEndpoint_VerifyCoupon =  "/verify_coupon";
 const String SubscriptionsHttpRequestEndpoint_CreateOrder =  "/subscription";
 const String SubscriptionsHttpRequestEndpoint_CheckOrderStatus =  "/subscription_payment_status";
-const String SubscriptionsHttpRequestEndpoint_CheckDateAvailability =  "/subscription_payment_status";
+const String SubscriptionsHttpRequestEndpoint_CheckDateAvailability =  "/subscription_start_date_validity";
 const String SubscriptionsHttpRequestEndpoint_ActivateSubscription =  "/activate_subscription";
