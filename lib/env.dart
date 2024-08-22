4a0ce8aaf80784e561beec23a2ed1e8afe1e37c2
Prasanth K M <prasanthkm49@gmail.com>
2024-08-22 12:42:21 +0530

    update app ids

--- lib/env.dart
+++ lib/env.dart
@@ -4,4 +4,4 @@ const apiEndPoint = "vertexlabs.online";
 const clientId = "6xnfBLxO7cOyZ2tQeU7nQGWtT1zLXq";
 const clientSecret = "AmJVWEDv3Q8Je1ZmPdyiS7ydBJiPNW";
 const appStorePackageId = "com.codeox.doneapp";
-const playStorePackageId = "com.nutribites.food_delivery_diet_app";
+const playStorePackageId = "com.codeox.doneapp";
